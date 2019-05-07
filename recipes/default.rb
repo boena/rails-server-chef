8bcc551b767616a48cc6d3e45b3ceaa15fac5b77
Björn Lennartsson <bjorn@ratherunique.se>
2019-05-07 17:09:50 +0200

    Nginx setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,32 @@ packages = %w(
 
 packages.each { |name| package name }
 
+###############
+# NGINX
+###############
+
+template "/etc/nginx/nginx.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "nginx.conf.erb"
+end
+
+# Open ports
+bash "allowing nginx traffic through firewall" do
+  user "root"
+  code "ufw allow 'Nginx Full'"
+end
+
+service 'nginx' do
+  provider Chef::Provider::Service::Systemd
+  supports :restart => true
+  action :restart
+end
+
+###############
 # Setup Ruby
+###############
 
 global_version = node[:rbenv][:global] || '2.6.0'
 
