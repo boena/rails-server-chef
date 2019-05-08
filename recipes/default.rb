0c8c2e2c5a3ddb092acd1936479b452e7200590b
Björn Lennartsson <bjorn@ratherunique.se>
2019-05-08 17:01:41 +0200

    Node + Yarn

--- recipes/default.rb
+++ recipes/default.rb
@@ -58,3 +58,27 @@ versions.each do |version|
 end
 
 rbenv_rehash global_version
+
+
+###############
+# Setup Node and Yarn
+###############
+
+bash 'node' do
+  user 'root'
+  code <<-EOC
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    apt-get update
+    apt-get install -y nodejs
+  EOC
+end
+
+bash 'yarn' do
+  user 'root'
+  code <<-EOC
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    apt-get update
+    apt-get install yarn
+  EOC
+end
