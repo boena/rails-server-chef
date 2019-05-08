efe8c25cc666240c1451562090343c2c58542326
Björn Lennartsson <bjorn@ratherunique.se>
2019-05-08 15:26:16 +0200

    Added Postgres user config options

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -6,6 +6,10 @@ if !node[:postgres] or !node[:postgres][:password]
   raise ArgumentError.new('You need to provide a password for Postgres.')
 end
 
+if !node[:postgres] or !node[:postgres][:users] or !node[:postgres][:users].any?
+  raise ArgumentError.new('You need to provide at least one local user.')
+end
+
 postgresql_server_install 'PostgreSQL Server install' do
   action :install
 end
@@ -13,4 +17,13 @@ end
 postgresql_server_install 'Setup PostgreSQL server' do
   password node[:postgres][:password]
   action :create
+end
+
+node[:postgres][:users].each do |user|
+  postgresql_user user[:username] do
+    password user[:password]
+    createdb user[:create_db] || true
+    createrole user[:create_role] || false
+    superuser user[:superuser] || false
+  end
 end
