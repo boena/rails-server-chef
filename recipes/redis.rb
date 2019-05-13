18d2d46fc6018dde3e1c5225920f1d2d1ffe1343
Björn Lennartsson <bjorn@ratherunique.se>
2019-05-13 10:46:54 +0200

    Template files for redis and memcached. Possibility to set password for redis

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -1,3 +1,25 @@
 apt_update
 
 package 'redis-server'
+
+template "/etc/redis/redis.conf" do
+  owner "redis"
+  group "redis"
+  mode "0640"
+  source "redis.conf.txt"
+end
+
+if node[:redis][:password]
+  bash 'set redis password' do
+    user 'root'
+    code <<-EOC
+      "sed -i 's/^# requirepass foobared/requirepass #{node[:redis][:password]}/g' /etc/redis/redis.conf"
+    EOC
+  end
+end
+
+service 'redis' do
+  provider Chef::Provider::Service::Systemd
+  supports :restart => true
+  action :restart
+end
