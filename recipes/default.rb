a28b828d99f0867641aa386624627b5a916b3b97
Björn Lennartsson <bjorn@ratherunique.se>
2019-06-25 13:36:34 +0200

    Check if node and yarn is installed.

--- recipes/default.rb
+++ recipes/default.rb
@@ -67,24 +67,32 @@ rbenv_rehash global_version
 
 # TODO: Fix so it check if exists before downloading every time
 
-# bash 'node' do
-#   user 'root'
-#   code <<-EOC
-#     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-#     apt-get update
-#     apt-get install -y nodejs
-#   EOC
-# end
-#
-# bash 'yarn' do
-#   user 'root'
-#   code <<-EOC
-#     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-#     apt-get update
-#     apt-get install yarn
-#   EOC
-# end
+if !File.exist?('/usr/bin/node') and !File.exist?('/usr/local/bin/node')
+  bash 'node' do
+    user 'root'
+    code <<-EOC
+      curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+      apt-get update
+      apt-get install -y nodejs
+    EOC
+  end
+else
+  p 'NodeJS already installed, skipping...'
+end
+
+if !File.exist?('/usr/bin/yarn') and !File.exist?('/usr/local/bin/yarn')
+  bash 'yarn' do
+    user 'root'
+    code <<-EOC
+      curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+      echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+      apt-get update
+      apt-get install yarn
+    EOC
+  end
+else
+  p 'Yarn already installed, skipping...'
+end
 
 
 ###############
